d445c178a36cafcbf56f48584c565fd6d88dcf6b
NFLSTY <naufalsatya10@gmail.com>
2026-06-30 22:34:43 +0700

    feat: enable CORS for the application with specified origin and methods

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,15 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  //
+  // ENABLE CORS
+  //
+  app.enableCors({
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 
